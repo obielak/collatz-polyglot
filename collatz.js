b04c46ba3d3d4b135d5ef234c6e4f71ae184d4e0
bielak <obielak@LibiMac02-11.local>
2014-05-19 13:46:48 -0700

    ghdhdh

--- collatz.js
+++ collatz.js
@@ -1,1 +1,20 @@
 console.log('Oliver Bielak')
+function collatz(x) {
+	var length = 0;
+	console.log(x)
+	while (x > 1) {
+		if (x % 2 == 0) {
+			x = x / 2
+		}
+		else {
+			x = 3*x + 1 
+		}
+		console.log(x)
+		length +=1		
+	}
+	return length 
+}
+
+var start = 59; 
+length = collatz(start)
+console.log("Collatz ("+String(start)+") has a  length of ("+String(length)+")") 
